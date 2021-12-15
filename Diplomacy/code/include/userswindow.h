af607aef7d1d5f4063c55702b14fa722d694184a
ulquiorra 68 <milicagolubovic13@gmail.com>
2021-12-15 17:35:09 +0100

    forma za unos igraca

---
+++ Diplomacy/code/include/userswindow.h
@@ -0,0 +1,28 @@
+#ifndef USERSWINDOW_H
+#define USERSWINDOW_H
+
+#include <QDialog>
+#include "code/include/mainwindow.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class UsersWindow; }
+QT_END_NAMESPACE
+
+class UsersWindow : public QDialog
+{
+    Q_OBJECT
+
+public slots:
+    void onOk();
+    void onBack();
+
+public:
+    UsersWindow(MainWindow *parent = nullptr);
+    ~UsersWindow();
+
+
+private:
+    Ui::UsersWindow *ui;
+    MainWindow* mw;
+};
+#endif // USERSWINDOW_H
