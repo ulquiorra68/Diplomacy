6bffea424938bbd4378a1e56c42d90ba750133ea
nbulatovic97 <nbulatovic97@gmail.com>
2021-12-19 18:11:27 +0100

    Added Territory class, nation & color enum

---
+++ Diplomacy/Color.h
@@ -0,0 +1,10 @@
+//color enum hex value (R,G,B,A)
+enum Color {
+    Red    = 0xff0000ff,
+    Green  = 0x00ff00ff,
+    Blue   = 0x0000ffff,
+    Yellow = 0xffff00ff,
+    Cyan   = 0x00ffffff,
+    Black  = 0x000000ff,
+    White  = 0xffffffff
+};
---
+++ Diplomacy/Territory.cpp
@@ -0,0 +1,50 @@
+#include "Territory.h"
+
+using namespace std;
+
+Territory::Territory(string name, Nation nation, double x, double y, bool isCapital)
+{
+	m_TerritoryrName = name;
+	m_TerritoryNationalBelonging = nation;
+	m_PositionX = x;
+	m_PositionY = y;
+	m_CapitalFlag = isCapital;
+}
+
+string Territory::GetNationalBelongingText()
+{
+	string nation_string;
+	switch (m_TerritoryNationalBelonging) {
+	case Russia:
+		nation_string = "Russia";
+		break;
+	case Turkey:
+		nation_string = "Turkey";
+		break;
+	case Germany:
+		nation_string = "Germany";
+		break;
+	case Austia:
+		nation_string = "Austria";
+		break;
+	case France:
+		nation_string = "France";
+		break;
+	case England:
+		nation_string = "England";
+		break;
+	case Italy:
+		nation_string = "Italy";
+		break;
+	default:
+		nation_string = "Unknown National Belonging";
+		break;
+	}
+
+	return nation_string;
+}
+
+void Territory::AddBorders(vector<Territory*> territorries) {
+
+	m_Borders = territorries;
+}
